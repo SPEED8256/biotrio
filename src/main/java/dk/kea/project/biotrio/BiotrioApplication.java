d0e304f0993695943be7dcfa61d1dc2af729099d
Alin Plamadeala <alin.plamadeala@outlook.com>
2019-04-30 11:31:43 +0200

    Initial commit

---
+++ src/main/java/dk/kea/project/biotrio/BiotrioApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.project.biotrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiotrioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiotrioApplication.class, args);
+    }
+
+}
