a5b5b8e74d22bc8e65fa79f31b87ade6c3c182fa
pedro <pmcpalma@gmail.com>
2019-05-18 17:50:58 +0200

    testing customer login page

--- src/main/java/dk/kea/project/biotrio/Controller/LoginController.java
+++ src/main/java/dk/kea/project/biotrio/Controller/LoginController.java
@@ -1,15 +1,21 @@
 package dk.kea.project.biotrio.Controller;
 
-
-import dk.kea.project.biotrio.Domain.Customer;
+import dk.kea.project.biotrio.Customer;
+import dk.kea.project.biotrio.LoginRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 public class LoginController {
+
+    //instance of repository class
+    private LoginRepository loginRepo;
+
     //to get login page
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String getLoginForm() {
@@ -19,11 +25,18 @@ public class LoginController {
     //checking for login credentials
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String login(@ModelAttribute("customerForm") Customer customer, Model model) {
-        if (customer.getUsername().equals("admin") && customer.getPassword().equals("admin")) {
+
+        List<Customer> customerListUP = loginRepo.findAllCustomers();
+
+        for (Customer customerObj : customerListUP) {
+            if (customerObj.getUsername().equals(customer.getUsername())
+                    && customerObj.getPassword().equals(customer.getPassword())) {
+            }
             //should return a booking page, where the customer (now logged in) continues to book a ticket.
             // As of now returns a test page
             return "login-home";
         }
+
         model.addAttribute("invalidCredentials", true);
 
         return "customer-login";
--- src/main/java/dk/kea/project/biotrio/LoginRepository.java
+++ src/main/java/dk/kea/project/biotrio/LoginRepository.java
@@ -1,16 +1,41 @@
 package dk.kea.project.biotrio;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class LoginRepository {
 
+    @Autowired
+    private JdbcTemplate jdbc;
 
+    //method goes through db customer table. Creates an object for each customer "username" and "password"
+    //and adds the created objects to a list.
+    //returns list
 
+    public List<Customer> findAllCustomers() {
+        SqlRowSet rs = jdbc.queryForRowSet("SELECT * FROM customer");
+        List<Customer> customerListUP = new ArrayList<>();
+        while (rs.next()) {
+            Customer customer = new Customer();
+            customer.setUsername(rs.getString("username"));
+            customer.setPassword(rs.getString("password"));
 
-
+            customerListUP.add(customer);
+        }
+        return customerListUP;
+    }
 }
+
+
+
+
+
 
 
 
